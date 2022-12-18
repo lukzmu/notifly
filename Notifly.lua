b8c546267b0fd7d710c288207a119a1384f39c8b
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2022-12-18 12:02:55 +0100

    Make whisper sound louder

--- Notifly.lua
+++ Notifly.lua
@@ -1,5 +1,6 @@
 local Notifly = CreateFrame("Frame");
+
 Notifly:RegisterEvent("CHAT_MSG_WHISPER");
 Notifly:SetScript("OnEvent", function (self, event, msg, sender)
-    PlaySound(111366);
+    PlaySoundFile("Interface\\Addons\\Notifly\\message.ogg", "Master");
 end);
