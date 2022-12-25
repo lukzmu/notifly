29c85e4138975469226b951585b20112d83e64e5
Łukasz Żmudziński <80778518+lukzmu@users.noreply.github.com>
2022-12-25 22:33:45 +0000

    Fix battle net whisper not playing sound

--- Notifly.lua
+++ Notifly.lua
@@ -1,6 +1,7 @@
 local Notifly = CreateFrame("Frame");
 
 Notifly:RegisterEvent("CHAT_MSG_WHISPER");
+Notifly:RegisterEvent("CHAT_MSG_BN_WHISPER");
 Notifly:SetScript("OnEvent", function (self, event, msg, sender)
     PlaySoundFile("Interface\\Addons\\Notifly\\message.ogg", "Master");
 end);
